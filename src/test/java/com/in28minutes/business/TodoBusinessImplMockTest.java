d26a5d9f7d89342628a6787ded3e51a97671ff7f
Anshul <anshul524@gmail.com>
2020-05-23 16:43:31 +0530

    verifying mocks

--- src/test/java/com/in28minutes/business/TodoBusinessImplMockTest.java
+++ src/test/java/com/in28minutes/business/TodoBusinessImplMockTest.java
@@ -3,7 +3,11 @@ package com.in28minutes.business;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.atLeast;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.Arrays;
@@ -49,5 +53,25 @@ public class TodoBusinessImplMockTest {
 		//Then
 		assertThat(filterTodos.size(),is(3));
 	}
-
+	
+	@Test
+	public void testTodosNotRelatedToSpring_usingAMock_BDD() {
+		//Given
+		TodoService mockTodoService = mock(TodoService.class);
+		List<String> asList = Arrays.asList("Learn Spring MVC","Learn Spring","Learn to Dance" ,"Spring AOP");
+		given(mockTodoService.retrieveTodos("Dummy")).willReturn(asList);
+		TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(mockTodoService); 
+		
+		//When
+		todoBusinessImpl.deleteNotRelatedToSpring("Dummy");
+		
+		//Then-1
+		verify(mockTodoService).deleteTodo("Learn to Dance");
+		//Then-2
+		verify(mockTodoService,times(1)).deleteTodo("Learn to Dance");
+		//Then-3
+		verify(mockTodoService,atLeast(1)).deleteTodo("Learn to Dance");
+		//Then-4
+		verify(mockTodoService,never()).deleteTodo("Learn Spring MVC");
+	}
 }
